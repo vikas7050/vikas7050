196aa62c9dc526b9dfabbd3825bba0bf2875dc86
vikash kumar yadav <113546659+vikas7050@users.noreply.github.com>
2023-02-20 23:12:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+import express from 'express';
+import cors from 'cors'
 
+const app = express();
+
+// app.use(cors());
+app.use(express.json())
+// a=endpoint,
+// after endpoint all other entities are middlewares => 
+// app.get(a,b,c)
+app.get('/hello', (req, res,next) => {
+    console.log(req)
+
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('X-middleware-example','true'); 
+
+    if(typeof(req.query.hello) === 'number'){
+        res.status(200).json({type:'string'})
+    }else{
+
+        next();
+    }
+    // if(req.query.hello )
+
+    // res.status(200).json({ result: "Server working ocrrectly" })
+},(req,res,next)=>{
+    console.log(next)
+    res.status(200).json({ result: "Server working ocrrectly" })
+})
+
+
+app.listen('5000', () => {
+    console.log("Server running at port 5000")
+})
